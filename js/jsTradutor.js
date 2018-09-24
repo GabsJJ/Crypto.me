8069f740766ec8b056670ce6ed63c1b0e996c422
PedroMReno <pedrohmreno@gmail.com>
2018-09-24 10:25:32 -0300

    Atualização no tradutor

--- js/jsTradutor.js
+++ js/jsTradutor.js
@@ -11,6 +11,7 @@ function criptografar(mensagem, idCripto)
     {
         case 1:
             nMensagem = cA1Z26(mensagem);
+            break;
             
         default:
             nMensagem = mensagem;
@@ -21,119 +22,127 @@ function criptografar(mensagem, idCripto)
 }
 
 /* Criptografar */
-function cA1Z26 (mensangem)
+function cA1Z26 (mensagem)
 {
-    for(var i in mensagem)
+    var resul = "";
+    for(var i = 0; i < mensagem.length; i++)
     {
-        var resul = "";
+        var letra = mensagem.charAt(i);
         
-        switch(mensagem[i])
+        switch(letra)
         {
             case "A":
-                resul = "1";
+                resul += "1";
                 break;
                 
             case "B":
-                resul = "2";
+                resul += "2";
                 break;
             
             case "C":
-                resul = "3";
+                resul += "3";
                 break;
                 
             case "D":
-                resul = "4";
+                resul += "4";
                 break;
                 
             case "E":
-                resul = "5";
+                resul += "5";
                 break;
                 
             case "F":
-                resul = "6";
+                resul += "6";
                 break;
                 
             case "G":
-                resul = "7";
+                resul += "7";
                 break;
                 
             case "H":
-                resul = "8";
+                resul += "8";
                 break;
                 
             case "I":
-                resul = "9";
+                resul += "9";
                 break;
                 
             case "J":
-                resul = "10";
+                resul += "10";
                 break;
                 
             case "K":
-                resul = "11";
+                resul += "11";
                 break;
                 
             case "L":
-                resul = "12";
+                resul += "12";
                 break;
                 
             case "M":
-                resul = "13";
+                resul += "13";
                 break;
                 
             case "N":
-                resul = "14";
+                resul += "14";
                 break;
                 
             case "O":
-                resul = "15";
+                resul += "15";
                 break;
                 
             case "P":
-                resul = "16";
+                resul += "16";
                 break;
                 
             case "Q":
-                resul = "17";
+                resul += "17";
                 break;
                 
             case "R":
-                resul = "18";
+                resul += "18";
                 break;
                 
             case "S":
-                resul = "19";
+                resul += "19";
                 break;
                 
             case "T":
-                resul = "20";
+                resul += "20";
                 break;
                 
             case "U":
-                resul = "21";
+                resul += "21";
                 break;
                 
             case "V":
-                resul = "22";
+                resul += "22";
                 break;
                 
             case "W":
-                resul = "23";
+                resul += "23";
                 break;
                 
             case "X":
-                resul = "24";
+                resul += "24";
                 break;
                 
             case "Y":
-                resul = "25";
+                resul += "25";
                 break;
                 
             case "Z":
-                resul = "26";
+                resul += "26";
+                break;
+
+            case " ":
+                resul = resul.substring(0, resul.length - 1);
+                resul += " ";
                 break;
         }
-        
-        
+        if(letra != " ")
+            resul += "-";
     }
+
+    return resul.substring(0, resul.length - 1);
 }
