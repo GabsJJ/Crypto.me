53c8e73d2ade21f70b64bf2b4815baaffd945137
PedroMReno <pedrohmreno@gmail.com>
2018-10-16 11:56:04 -0300

    BD
    -Sincronizando o Quiz com o banco de dados
    -Arrumado outros erros

--- Banco de dados/Tables.sql
+++ Banco de dados/Tables.sql
@@ -34,7 +34,7 @@ questao varchar(200) not null,
 resposta varchar(200) not null
 )
 
---Colocando dodos
+--Colocando dodos0
 --Categorias
 insert into CriptoCategorias values(1, 'Simples'  , 'Criptografias mais fáceis de entender, compostas de algoritmos de poucas etapas')
 insert into CriptoCategorias values(2, 'Complexas', 'Criptografias mais complexas de entender, compostas de algoritmos de várias etapas')
@@ -46,5 +46,5 @@ insert into Criptografia values(1, 'A1Z26','Essa é uma criptografia simples onde
 entre cada palavra. Acentos não são usados e não há diferença entre letras maiúsculas e minúsculas',2,1)
 
 --Quiz
-insert into quiz values(1,'Criptografe "Ola mundo" usando A1Z26', '15-12-1 13-21-14-4-15')
-insert into quiz values(2,'Descriptografe "3-18-9-16-20-15-7-18-1-6-9-1" usando A1Z26', 'Criptografia')
+insert into Quiz values(1,'Criptografe "Ola mundo" usando A1Z26', '15-12-1 13-21-14-4-15')
+insert into Quiz values(2,'Descriptografe "3-18-9-16-20-15-7-18-1-6-9-1" usando A1Z26', 'Criptografia')
