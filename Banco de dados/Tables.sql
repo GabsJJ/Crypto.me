e54649243ef2c68a54966ce1a8c0dba6834b44fb
GabsJJ <38597996+GabsJJ@users.noreply.github.com>
2018-11-07 11:17:29 -0200

    BD Images

--- Banco de dados/Tables.sql
+++ Banco de dados/Tables.sql
@@ -46,31 +46,24 @@ insert into Criptografia values(1, 'A1Z26','Essa é uma criptografia simples onde
 'Cada letra será substituida pelo número da sua posição no alfabeto. Para que os números não se misturem, se usa "-" entre cada letra e um espaço
 entre cada palavra. Acentos não são usados e não há diferença entre letras maiúsculas e minúsculas.',2,1)
 
-insert into Criptografia values(2,'Cifra de César','Esta é uma das mais antigas e mais simples forma de criptografia existente.',
-'A Cifra de César consiste basicamente em um processo de substituição com base em um número que indica quantas posições o alfabeto original
+insert into Criptografia values(2,'Cifra de César','A Cifra de César consiste basicamente em um processo de substituição com base em um número que indica quantas posições o alfabeto original
 vai se deslocar. Um exemplo, se esse número for igual a 3, o alfabeto vai se deslocar em 3 posições, ou seja, a letra A passa a ser D, B passa
-a ser E, e assim por diante.',1,2)
+a ser E, e assim por diante.
+<p><b>Exemplo da troca de alfabetos: </b></p>
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Caesar3.svg/640px-Caesar3.svg.png">',1,2)
 
 insert into Criptografia values(3,'Combined Cipher','Esta criptografia é uma mistura de A1Z26 com Cifra de Cesar na rotação nº 22.',
 'Para utilizar o Combined Cipher deve-se primeiro criptografar a mensagem com Cifra de Cesar na rotação nº 22, depois criptografar
 o resultado com A1Z26. Ambas essas criptografias estão explicados nesse mesmo site.',1,3)
 
-insert into Criptografia values(4,'Binário','Essa é uma forma de criptografia na qual se substitui a base de um número decimal de 10 para 2',
-'Nâo é muito usada como criptografia, já que é, de certa forma, uma línguagem que os computadores 
+insert into Criptografia values(4,'Binário','Nâo é muito usada como criptografia, já que é, de certa forma, uma línguagem que os computadores 
 utilizam para se "comunicar"; porém, nada impede que seja usada com criptografia. Para converter um número para binário, primeiro, faça uma 
 divisão inteira por 2 (Ex 25 / 2 = 12), segundo, pegue o resto da divisão do numero por 2 (Ex: 25 % 2 = 1) e guarde-o em algum lugar. 
 E para o quarto passo, basta repetir esse processo com o resultado que foi encontrado na divisão inteira até que este seja igual = 1. 
-Depois disso, você terá de juntar todos os restos e ordená-los de trás pra frente. Para um exemplo em vídeo, <a href="https://www.youtube.com/watch?v=mttrG_kbHN4">Clique aqui.</a>
-<img src="imagens/dPb.jpg">
-<p><b>Dica:</b></p> Para converter letras para binário, pesquise sobre a <a href="https://pt.wikipedia.org/wiki/ASCII">Tabela ASCII</a>',
+Depois disso, você terá de juntar todos os restos e ordená-los de trás pra frente. Para um exemplo em vídeo, <a href="https://www.youtube.com/watch?v=mttrG_kbHN4" target="_blank">Clique aqui.</a>
+<img src="https://3.bp.blogspot.com/-cANHx4dx_LA/TdZsKqAUvqI/AAAAAAAAAV8/E5xQCFws5rY/s1600/decimal+para+bin%25C3%25A1rio.jpg">
+<p><b>Dica:</b></p> Para converter letras para binário, pesquise sobre a <a href="https://pt.wikipedia.org/wiki/ASCII" target="_blank">Tabela ASCII</a>',
 3,2)
-
-update Criptografia set explicacao = 'A Cifra de César consiste basicamente em um processo de substituição com base em um número que indica quantas posições o alfabeto original
-vai se deslocar. Um exemplo, se esse número for igual a 3, o alfabeto vai se deslocar em 3 posições, ou seja, a letra A passa a ser D, B passa
-a ser E, e assim por diante.
-<p><b>Exemplo da troca de alfabetos: </b></p>
-<img src="imagens/caesar.png">'
-where id = 2
 
 select * from Criptografia
 
